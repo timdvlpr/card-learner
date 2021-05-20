17dc151b0f74d64fb88062ddbe521075a2706ca8
timdvlpr <81222680+timdvlpr@users.noreply.github.com>
2021-05-20 15:14:35 +0200

    Group Model + Service für Serverkommunikation hinzugefügt (CRUD Funktionalität)

---
+++ client/src/app/components/group/group.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { Group } from './group.model';
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GroupService {
+
+  API_URL = 'http://localhost:5000/api/group'
+  groups: Group[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  async getGroups(): Promise<void> {
+    await this.http.get(`${this.API_URL}/all`)
+      .toPromise()
+      .then((groups: any) => this.groups = groups.data)
+      .catch(e => {
+        throw e.error.message;
+      });
+  }
+
+  async createGroup(data: any): Promise<void> {
+    await this.http.post(this.API_URL, data)
+      .toPromise()
+      .then(() => this.getGroups())
+      .catch(e => {
+        throw e.error.message;
+      });
+  }
+
+  async updateGroup(id: number, data: any): Promise<void> {
+    await this.http.put(`${this.API_URL}/${id}`, data)
+      .toPromise()
+      .then(() => this.getGroups())
+      .catch(e => {
+        throw e.error.message;
+      });
+  }
+
+  async deleteGroup(id: number): Promise<void> {
+    await this.http.delete(`${this.API_URL}/${id}`)
+      .toPromise()
+      .then(() => this.getGroups())
+      .catch(e => {
+        throw e.error.message;
+      });
+  }
+
+}
