c62100b8c78317b2e096f004d98e0d30c1e06a60
timdvlpr <81222680+timdvlpr@users.noreply.github.com>
2021-05-21 09:38:14 +0200

    Modules angepasst, Home Route hinzugefügt

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,15 +2,40 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { SidebarComponent } from './components/layout/sidebar/sidebar.component';
+import { GroupListComponent } from './components/group/group-list/group-list.component';
+import { StackItemComponent } from './components/stack/stack-item/stack-item.component';
+import { HomeComponent } from './views/home/home.component';
+import { AddEditModalComponent } from './components/modal/add-edit-modal/add-edit-modal.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { StackFormComponent } from './components/stack/stack-form/stack-form.component';
+import { GroupFormComponent } from './components/group/group-form/group-form.component';
+import { CardFormComponent } from './components/card/card-form/card-form.component';
+import { AlertComponent } from './components/alert/alert/alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    GroupListComponent,
+    StackItemComponent,
+    HomeComponent,
+    AddEditModalComponent,
+    StackFormComponent,
+    GroupFormComponent,
+    CardFormComponent,
+    AlertComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
