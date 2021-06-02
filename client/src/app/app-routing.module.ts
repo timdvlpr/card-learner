09d21059ca1b42de9fad94ae4cbbb35c6350e3cb
timdvlpr <81222680+timdvlpr@users.noreply.github.com>
2021-06-02 15:42:53 +0200

    Laden der richtigen "add" und "edit" Routen hinzugefügt

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { CardsComponent } from './views/cards/cards.component';
+import { LearnComponent } from './views/learn/learn.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'add/:addType', component: HomeComponent },
+  { path: 'edit/:editType/:slug', component: HomeComponent },
   { path: 'cards', component: CardsComponent },
+  { path: 'learn/:id', component: LearnComponent },
   { path: '**', redirectTo: '' }
 ];
 
--- client/src/app/views/home/home.component.ts
+++ client/src/app/views/home/home.component.ts
@@ -1,17 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { StackService } from '../../components/stack/stack.service';
 import { ModalService } from '../../components/modal/modal.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { GroupService } from '../../components/group/group.service';
+import { CardService } from '../../components/card/card.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnDestroy {
+
+  private subRoute: Subscription;
 
   constructor(
     public stackService: StackService,
-    public modalService: ModalService
-  ) { }
+    private groupService: GroupService,
+    private cardService: CardService,
+    public modalService: ModalService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.subRoute = this.route.params.subscribe(params => {
+      if (params['addType']) {
+        this.checkAddRoute(params['addType']);
+      }
+      if (params['editType'] && params['slug']) {
+        this.checkEditRoute(params['editType'], params['slug']);
+      }
+    });
+  }
+
+  checkAddRoute(type: string): void {
+    switch (type) {
+      case 'stack':
+        this.modalService.showAddModal('stack');
+        break;
+      case 'group':
+        this.modalService.showAddModal('group');
+        break;
+      case 'card':
+        this.modalService.showAddModal('card');
+        break;
+      default:
+        this.router.navigate(['home']);
+        break;
+    }
+  }
+
+  checkEditRoute(type: string, slug: string): void {
+      switch (type) {
+        case 'stack':
+          this.stackService.getStack(slug)
+            .then(() => this.modalService.showEditModal('stack', this.stackService.stack));
+          break;
+        case 'group':
+          this.groupService.getGroup(slug)
+            .then(() => this.modalService.showEditModal('group', this.groupService.group));
+          break;
+        default:
+          this.router.navigate(['home']);
+          break;
+      }
+  }
+
+  ngOnDestroy() {
+    this.subRoute.unsubscribe();
+  }
 
 }
