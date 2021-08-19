c3d4c200c35dd6e230091c8ccadc2b29cbfb6f90
timdvlpr <81222680+timdvlpr@users.noreply.github.com>
2021-08-19 15:05:37 +0200

    Fix: Interface -> Klasse

--- client/src/app/components/stack/stack.model.ts
+++ client/src/app/components/stack/stack.model.ts
@@ -1,6 +1,13 @@
-export interface Stack {
+export class Stack {
   id: number;
   name: string;
   slug: string;
   inGroup: number;
+
+  constructor(id: number, name: string, slug: string, inGroup: number) {
+    this.id = id;
+    this.name = name;
+    this.slug = slug;
+    this.inGroup = inGroup;
+  }
 }
