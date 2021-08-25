b300571be2b3a89e467426d29b5b6732db4f8892
timdvlpr <81222680+timdvlpr@users.noreply.github.com>
2021-08-25 15:13:52 +0200

    Smart Modal Abhängigkeit hinzugefügt, unbenutzte Dateien gelöscht, Card Model (Interface -> Class)

--- client/src/app/components/card/card.model.ts
+++ client/src/app/components/card/card.model.ts
@@ -1,8 +1,16 @@
-export interface Card {
+export class Card {
   id: number;
   question: string;
   answer: string;
   inStack: number;
   slug: string;
   name?: string;
+
+  constructor(id: number, question: string, answer: string, inStack: number, slug: string) {
+    this.id = id;
+    this.question = question;
+    this.answer = answer;
+    this.inStack = inStack;
+    this.slug = slug;
+  }
 }
