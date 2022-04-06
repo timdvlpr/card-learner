6ca0e1c9931bb73dd51b6d4801a0212aa57988c2
timdvlpr <81222680+timdvlpr@users.noreply.github.com>
2022-04-06 20:59:58 +0200

    alert animation added

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 import { AlertService } from './components/alert/alert.service';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('alertAnimation', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(-1rem)' }),
+        animate('200ms', style({ opacity: 1, transform: 'translateY(0)' }))
+      ]),
+      transition(':leave', [
+        style({ transform: 'translateY(0)' }),
+        animate('200ms', style({ opacity: 0, transform: 'translateY(-1rem)' }))
+      ])
+    ])
+  ]
 })
 export class AppComponent {
   constructor(public alertService: AlertService) {}
