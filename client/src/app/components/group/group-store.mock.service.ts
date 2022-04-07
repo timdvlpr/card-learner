5bb101d16e3fb9ea1c1ce92d1185d56e959dad8d
timdvlpr <81222680+timdvlpr@users.noreply.github.com>
2022-04-07 14:02:47 +0200

    store mock services added for testing

---
+++ client/src/app/components/group/group-store.mock.service.ts
@@ -0,0 +1,38 @@
+import { BehaviorSubject } from 'rxjs';
+import { Group } from './group.model';
+
+export class GroupStoreMockService {
+  groupSource = new BehaviorSubject<Group[]>([]);
+  groups$ = this.groupSource.asObservable();
+
+  selectedGroupSource = new BehaviorSubject<number>(-1);
+  selectedGroup$ = this.selectedGroupSource.asObservable();
+
+  updateSelectedGroup(id: number): void {
+    this.selectedGroupSource.next(id);
+  }
+
+  getAll(): Group[] {
+    return this.groupSource.getValue();
+  }
+
+  add(group: Group): void {
+    const groups = [...this.getAll(), group];
+    this.groupSource.next(groups);
+  }
+
+  update(id: number, group: Group): void {
+    const groups = this.getAll().map((g) => {
+      if (g.id === group.id) {
+        return new Group(group.name, group.id, group.slug);
+      }
+      return g;
+    });
+    this.groupSource.next(groups);
+  }
+
+  remove(id: number): void {
+    const groups = this.getAll().filter((group) => group.id !== id);
+    this.groupSource.next(groups);
+  }
+}
---
+++ client/src/app/components/stack/stack-store.mock.service.ts
@@ -0,0 +1,38 @@
+import { BehaviorSubject, of } from 'rxjs';
+import { Stack } from './stack.model';
+
+export class StackStoreMockService {
+  stackSource = new BehaviorSubject<Stack[]>([]);
+  stacks$ = of([]);
+
+  selectedStackSource = new BehaviorSubject<number>(-1);
+  selectedStack$ = of(1);
+
+  getAll(): Stack[] {
+    return this.stackSource.getValue();
+  }
+
+  updateSelectedStack(id: number): void {
+    this.selectedStackSource.next(id);
+  }
+
+  add(stack: Stack): void {
+    const stacks = [...this.stackSource.getValue(), stack];
+    this.stackSource.next(stacks);
+  }
+
+  update(id: number, stack: Stack): void {
+    const stacks = this.getAll().map((s) => {
+      if (s.id === stack.id) {
+        return new Stack(stack.id, stack.name, stack.slug, stack.inGroup);
+      }
+      return s;
+    });
+    this.stackSource.next(stacks);
+  }
+
+  remove(id: number): void {
+    const stacks = this.getAll().filter((stack) => stack.id !== id);
+    this.stackSource.next(stacks);
+  }
+}
